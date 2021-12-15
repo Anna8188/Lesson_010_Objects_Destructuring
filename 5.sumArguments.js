1f272673998f51132881e21dc7f4e82249010b6b
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2021-12-15 05:38:29 +0400

    Pass arguments in sum function

---
+++ 5.sumArguments.js
@@ -0,0 +1,10 @@
+"use strict";
+// Task 5.
+//How to take arguments in sum function?
+function sum(x, y, z) {
+  return x + y + z;
+}
+const numbers = [1, 2, 3];
+const [x, y, z] = numbers;
+
+//console.log(sum(x, y, z)); // prints 6
