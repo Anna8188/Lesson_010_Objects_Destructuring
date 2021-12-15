9e5ca835e73f3ea904f093584f40de508f876feb
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2021-12-15 05:41:35 +0400

    Get name, country and job using destructuring

---
+++ 7.getValues.js
@@ -0,0 +1,10 @@
+"use strict";
+// Task 7
+// Get name, country and job using destructuring.
+
+let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+const { name, country, job } = person;
+
+//console.log(name); // "Sarah"
+//console.log(country); // "Nigeria"
+//console.log(job); // "Developer"
